a35be3bcec3abb5e799e089eee0a2a9c2ecda5d4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-03-17 21:44:13 +0100

    add functionality to toggle btw metric and imperial input fields visibility

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("BMI Calculator");
+// Select DOM Elements
+const metricOpt = document.getElementById("metric");
+const imperialOpt = document.getElementById("imperial");
+
+const metricHeight = document.getElementById("metric-height");
+const metricWeight = document.getElementById("metric-weight");
+
+const imperialFieldsContainer = document.querySelector(
+  ".imperial_input-wrapper",
+);
+const metricFieldsContainer = document.querySelector(".metric_input-wrapper");
+
+[metricOpt, imperialOpt].forEach((opt) => {
+  opt.addEventListener("click", () => {
+    if (opt.id === "metric") {
+      console.log("metric clicked!");
+      imperialFieldsContainer.classList.add("d-none");
+      imperialFieldsContainer.classList.remove("d-flex");
+      metricFieldsContainer.classList.add("d-flex");
+    } else if (opt.id === "imperial") {
+      console.log("imperial clicked!");
+      imperialFieldsContainer.classList.add("d-flex");
+      imperialFieldsContainer.classList.remove("d-none");
+      metricFieldsContainer.classList.add("d-none");
+      metricFieldsContainer.classList.remove("d-flex");
+    }
+  });
+});
